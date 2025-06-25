48c0111235c6e9f823eb334c5953d5cdb4be358d
Mansur-09595 <mansur_musaev_1997@mail.ru>
2025-06-25 13:24:22 +0200

    Deploy on S3 --v 1.2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,22 +114,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 if USE_S3:
-    INSTALLED_APPS += ["storages"]
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")  # ваш регион
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    AWS_DEFAULT_ACL = "public-read"
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    INSTALLED_APPS += ['storages']
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_DEFAULT_ACL = None
 
     # Статика всё ещё можно отдавать белайтноизом
-    STATIC_URL = "/staticfiles/"
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    STATICFILES_STORAGE = 'config.storage_backends.StaticStorage'
 
     # Медиаконтент в S3
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = 'config.storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = "/staticfiles/"
     STATIC_ROOT = BASE_DIR / "staticfiles"

e25e5a0ae454a5980b4cc85497a7426aea494ebf
Mansur-09595 <mansur_musaev_1997@mail.ru>
2025-06-26 00:42:28 +0200

    Deploy on S3 make staticfiles for admin in backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,6 +123,7 @@ if USE_S3:
     AWS_QUERYSTRING_AUTH = False
 
     # Статика всё ещё можно отдавать белайтноизом
+    STATIC_LOCATION = "static"
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     STATICFILES_STORAGE = 'config.storage_backends.StaticStorage'
 
--- backend/config/storage_backends.py
+++ backend/config/storage_backends.py
@@ -3,6 +3,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
+    file_overwrite = True
 
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
